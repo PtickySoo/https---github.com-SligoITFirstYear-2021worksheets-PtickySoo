3e8d13a0f244270f149ae60c826bd4184bada3fb
Christopher Gorman <78158751+PtickySoo@users.noreply.github.com>
2021-02-22 10:19:21 +0000

    first commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Q9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Varibles
+            double totalScores = 0;
+            double average;
+            int counter = 0;
+
+            double input =0;
+            do
+            {
+                Console.WriteLine("Enter test score!");
+                if(input != -999)
+                {
+                    totalScores += input;
+                    counter++
+                }
+            }
+            while (input != -999);
+
+            average = totalScores / counter;
+
+            Console.WriteLine("The average for {0} score is {1}", counter, average);
+        }
+    }
+}
